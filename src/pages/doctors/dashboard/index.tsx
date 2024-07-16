b1c33ab6dd9ee7c870e7565f363c9b0da1f0f92d
Majid <nastaran.samui@gmail.com>
2024-07-16 21:23:07 +0700

    update title

--- src/pages/doctors/dashboard/index.tsx
+++ src/pages/doctors/dashboard/index.tsx
@@ -33,7 +33,7 @@ const DashboardPage: NextPage = () => {
         <meta name='description' />
         <meta name="theme-color" />
         <meta name="emotion-insertion-point" content="" />
-        <title>Welcome to Distribution Live data</title>
+        <title>Welcome to Health care</title>
       </Head>
       <BreadCrumb subtitle='Dashboard' title='Dashboard' />
       <div className="content">
