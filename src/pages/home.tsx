fd7d100cf46c4a7eb423d318fa8ca0d82d1baa11
Majid Vezvaee <nastaran.samui@gmail.com>
2024-12-18 11:51:09 +0700

    home lighthouse update

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -27,6 +27,7 @@ import { updateUserProfile } from '@/redux/userProfile';
 import { updateHomeAccessToken } from '@/redux/homeAccessToken';
 import isJsonString from '@/helpers/isJson';
 import CookieConsentComponent from '@/components/shared/CookieConsentComponent';
+import { LazyLoadWrapper } from '.';
 
 
 const Home: NextPage = () => {
@@ -38,18 +39,20 @@ const Home: NextPage = () => {
         <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
         <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1" />
         <meta charSet='utf-8' />
-        <meta name='description' />
+        <meta name="description" content="We eager to bring health and health care service to you by easiest way that possible." />
         <meta name="theme-color" />
         <meta name="emotion-insertion-point" content="" />
         <title>Welcome to Health Care page</title>
       </Head>
       <HomeSearch />
       <LookingFor />
-      <HomeSpecialities />
-      <HomeBookDoctor />
-      <HomeFeatures />
-      <HomeBlog />
-      <Footer />
+      <LazyLoadWrapper>
+        <HomeSpecialities />
+        <HomeBookDoctor />
+        <HomeFeatures />
+        <HomeBlog />
+        <Footer />
+      </LazyLoadWrapper>
       <CookieConsentComponent />
       <ScrollToTop />
     </>
